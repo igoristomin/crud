cce1ba32f1213c89f80de1701bfa0c9a32b6318f
Igor Istomin <igoristomin.lic@yandex.ru>
2022-02-01 18:49:58 +0300

    refactor: impl CombineService

--- src/main/java/web/crud/controller/MainController.java
+++ src/main/java/web/crud/controller/MainController.java
@@ -8,21 +8,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import web.crud.model.Customer;
-import web.crud.service.CombineService;
+import web.crud.repository.CustomerRepository;
 import javax.validation.Valid;
 
 @Controller
 public class MainController {
 
     @Autowired
-    private CombineService combineService;
+    private CustomerRepository customerRepository;
 
 //    ------------------------------
 //    /
 //    ------------------------------
     @GetMapping("/")
     public String showIndex(Model model) {
-        return combineService.combineShowIndex(model);
+        model.addAttribute("customers", customerRepository.findAll());
+        return "index";
     }
 
 //    ------------------------------
@@ -35,7 +36,9 @@ public class MainController {
 
     @PostMapping("/create")
     public String postCreate(@Valid Customer customer, BindingResult result, Model model) {
-        return combineService.combinePostCreate(customer, result, model);
+        if (result.hasErrors()) return "create";
+        customerRepository.save(customer);
+        return "redirect:/";
     }
 
 //    ------------------------------
@@ -43,12 +46,20 @@ public class MainController {
 //    ------------------------------
     @GetMapping("/update/{id}")
     public String showUpdate(@PathVariable Long id, Model model) {
-        return combineService.combineShowUpdate(id, model);
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Record does not exist!"));
+        model.addAttribute("customer", customer);
+        return "update";
     }
 
     @PostMapping("/update/{id}")
     public String postUpdate(@PathVariable Long id, @Valid Customer customer, BindingResult result, Model model) {
-        return combineService.combinePostUpdate(id, customer, result, model);
+        if (result.hasErrors()) {
+            customer.setId(id);
+            return "update";
+        }
+        customerRepository.save(customer);
+        return "redirect:/";
     }
 
 //    ------------------------------
@@ -56,7 +67,10 @@ public class MainController {
 //    ------------------------------
     @GetMapping("/delete/{id}")
     public String runDelete(@PathVariable Long id, Model model) {
-        return combineService.combineRunDelete(id, model);
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Record does not exist!"));
+        customerRepository.delete(customer);
+        return "redirect:/";
     }
 
 }
